f672cd4f286e83066edc11044b004bbb0921dbdd
Cory <coryf65@gmail.com>
2021-01-24 15:18:50 -0600

    Player can now drive the Van

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 20f;
+    public float turnSpeed = 2f;
+
+    float horizontalInput = 0f;
+    float verticalInput = 0f;
 
     void Update()
     {
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
         // moving a 20 m/s
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
 
     }
 }
